b5d371087a9525a80fb33bfff0f8dc4c69c1a8cf
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 09:24:14 +0530

    Add files via upload

---
+++ adding values for common keys.py
@@ -0,0 +1,11 @@
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+dic={}
+for x in d1.keys():
+    for y in d2.keys():
+        if x==y:
+            dic[x]=d1[x]+d2[y]
+else:
+    dic[x]=d1[x]
+    dic[y]=d2[y]
+print(dic)
