6dc4e0212c7f379d95292aae6a35bbe90fe2ce7b
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 12:41:11 +0530

    Update check whether a given key already exists in a dictionary.py

--- check whether a given key already exists in a dictionary.py
+++ check whether a given key already exists in a dictionary.py
@@ -5,6 +5,5 @@ if "model" in car:
     print("Yes, 'model' is one of the keys in the car dictionary.")
 else:
     ("No,'model' key is not in dictionary")
-
-    
+  
 #Yes,'model' is one of the keys in the car dictionary.
