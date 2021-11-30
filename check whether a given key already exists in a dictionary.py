eb8f3c8c69b6de3e2ced6489c0ee954278e591ed
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 09:38:24 +0530

    Add files via upload

---
+++ check whether a given key already exists in a dictionary.py
@@ -0,0 +1,8 @@
+car={"brand":"ford",
+"model":"mustang",
+"year":1964}
+if "model" in car:
+    print("Yes, 'model' is one of the keys in the car dictionary.")
+else:
+    ("No,'model' key is not in dictionary")
+    
