a195e83107b4043b094f0d717d7b101a3945b951
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-11-30 09:36:25 +0530

    Add files via upload

---
+++ dictionaries to create a new one.py
@@ -0,0 +1,12 @@
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic=dict(dic1)
+dic.update(dic2)
+dic.update(dic3)
+for i,j in dic1.items():
+    for m,n in dic2.items():
+        for k,l in dic3.items():
+            if i==m:
+                dic[i]==j+m
+print(dic)
