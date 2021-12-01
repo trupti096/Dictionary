c48f3613d39cb132f43b9bb794b35fcdeb612b62
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-12-01 09:17:22 +0530

    Update get the key, value and item in a dictionary.py

--- get the key, value and item in a dictionary.py
+++ get the key, value and item in a dictionary.py
@@ -2,4 +2,13 @@ dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 count=0
 for i,j in dict_num.items():
     count=count+1
-    print(i,j,count)      
+    print(i,j,count)  
+
+
+# key  value  count
+# 1     10      1                                                                                               
+# 2     20      2                                                                                               
+# 3     30      3                                                                                               
+# 4     40      4                                                                                               
+# 5     50      5                                                                                               
+# 6     60      6
