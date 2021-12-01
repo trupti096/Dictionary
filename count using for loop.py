d38cee050bd4539a6e672d681dfd31af635ce7bd
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-12-01 15:42:40 +0530

    Add files via upload

---
+++ count using for loop.py
@@ -0,0 +1,41 @@
+a="MISSISSIPPI"
+n={}
+c=0
+for i in a:
+    a.split()
+    if i in n:
+        n[i]+=1
+    else:
+        n[i]=1
+print(n)
+
+# a="MISSISSIPPI"
+# i=0
+# d=0
+# str=""
+# s={}
+# while i<len(a):
+#     count=0
+#     j=0
+#     while j<len(a):
+#         if a[i]==a[j]:
+#             count=count+1
+#         j=j+1
+#     if a[i] not in str:
+#         str=str+a[i]
+#         d=count
+#         s[a[i]]=d
+#     i=i+1
+# print(s)
+
+
+a="MISSISSIPPI"
+n={}
+c=0
+for i in a:
+    a.split()
+    if i in n:
+        n[i]+=1
+    else:
+        n[i]=1
+print(n)
