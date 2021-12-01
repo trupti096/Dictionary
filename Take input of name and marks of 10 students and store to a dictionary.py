1ed61996e178ff0deb8fe0c806e1b1f9d96bdc65
trupti096 <84379470+trupti096@users.noreply.github.com>
2021-12-01 15:24:08 +0530

    Add files via upload

---
+++ Take input of name and marks of 10 students and store to a dictionary.py
@@ -0,0 +1,22 @@
+i=0
+a={ }
+while i<=10:
+    name=input("enter any name=")
+    marks=int(input("enter any numbers="))
+    a[name]=marks
+    i=i+1
+print(a)
+
+
+# dict={
+#     'sonu':85,
+#     'Kartik':90,
+#     'Deepak':96,
+#     'Aman':76,
+#     'Somesh':60,
+#     'Umesh':85,
+#     'Amarpal':70,
+#     'Roshan':57,
+#     'Riyaz':98,
+#     'Shabid':56
+# }   
